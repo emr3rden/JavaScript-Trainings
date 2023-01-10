f28c39b25abfec52a6e2550cdd7d162c1c35e93a
Emre Erden <emreerden@pm.me>
2023-01-10 23:30:15 +0300

    added sixth Questions-Algorithms

---
+++ Questions-Algorithms/Questions-Algorithms/6.js
@@ -0,0 +1,8 @@
+// Find the length of "Python" and "Jargon" and make a falsy comparison statement.
+
+let py = "Python";
+let jr = "Jargon";
+
+console.log(py.length, jr.length); // 6 6
+
+console.log(py.length === jr.length); // true
