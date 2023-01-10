63a3e6ed5b1b8172d139d5af12a8c22f1e78b579
Emre Erden <emreerden@pm.me>
2023-01-10 22:45:21 +0300

    added fifth Questions-Algorithms

---
+++ Questions-Algorithms/Questions-Algorithms/5.js
@@ -0,0 +1,13 @@
+// Figure out the result of the following comparison expression first without using console.log. After you decide the result confirm it using console.log.
+
+console.log(4 > 3); // true
+console.log(4 >= 3); // true
+console.log(4 < 3); // false
+console.log(4 <= 3); // false
+console.log(4 == 4); // true
+console.log(4 === 4); // true
+console.log(4 != 4); // false
+console.log(4 !== 4); // false
+console.log(4 != "4"); // false
+console.log(4 == "4"); // true
+console.log(4 === "4"); // true
