9f5ad4e7c615de772872e2e43eee839a943f3337
Emre Erden <emreerden@pm.me>
2023-01-11 23:09:53 +0300

    added eleventh Questions-Algorithms

---
+++ Questions-Algorithms/Questions-Algorithms/11.js
@@ -0,0 +1,48 @@
+// Create instances of Data Types using JavaScript Comparison Operators.
+
+console.log(3 > 2); // output: true, because 3 is greater than 2
+console.log(3 < 2); // output: false,  because 3 is greater than 2
+console.log(2 < 3); // output: true, because 2 is less than 3
+console.log(3 >= 2); // output: true, because 3 is greater than 2
+console.log(2 <= 3); // output: true, because 2 is less than 3
+console.log(3 == 2); // output: false, because 3 is not equal to 2
+console.log(3 != 2); // output: true, because 3 is not equal to 2
+console.log(3 != 3); // output: false, compare only value
+console.log(3 !== 3); // output: false, compare both value and data type
+
+//
+
+console.log(3 == "3"); // output: true, compare only value
+console.log(3 === "3"); // output: false, compare both value and data type
+console.log(3 !== "3"); // output: true, compare both value and data type
+
+console.log(0 == ""); // output: true, equivalent
+console.log(0 == " "); // output: true, equivalent
+console.log(0 === ""); // output: false, not exactly the same
+
+//
+
+console.log(0 == false); // output: true, equivalent
+console.log(0 === false); // output: false, not exactly the same
+console.log(1 == true); // output: true, equivalent
+console.log(1 === true); // output: false, not exactly the same
+
+//
+
+console.log(undefined == null); // output: true
+console.log(undefined === null); // output: false
+
+//
+
+console.log(NaN == NaN); // output: false, not equal
+console.log(NaN === NaN); // output: false
+
+//
+
+console.log("manga".length == "avocado".length); // output: false
+console.log("mango".length != "avocado".length); // output: true
+console.log("mango".length < "avocado".length); // output: true
+console.log("milk".length == "meat".length); // output: true
+console.log("milk".length != "meat".length); // output: false
+console.log("tomato".length == "potato".length); // output: true
+console.log("python".length > "dragon".length); // output: false
