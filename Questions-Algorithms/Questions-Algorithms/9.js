fc85005c61dd4f2a44e1aa53ae3a506cd534059e
Emre Erden <emreerden@pm.me>
2023-01-11 05:01:38 +0300

    added ninth Questions-Algorithms

---
+++ Questions-Algorithms/Questions-Algorithms/9.js
@@ -0,0 +1,19 @@
+// Find the lowest and highest value in the list using "Math.min()" and "Math.max()" using console.log.
+
+//
+
+// The Math.min() method returns the number with the lowest value.
+
+console.log(Math.min(-7, 44, -1, 0)); // output: -7
+console.log(Math.min(-5, 3, 20, 4, 5, 10)); // output: -5
+
+// returns the minimum value
+
+//
+
+// The Math.max() method returns the number with the highest value.
+
+console.log(Math.max(-5, 3, 20, 4, 5, 10)); // output: 20
+console.log(Math.max(1, 67, 11, 27, 19, 6)); // output: 67
+
+// returns the maximum value
