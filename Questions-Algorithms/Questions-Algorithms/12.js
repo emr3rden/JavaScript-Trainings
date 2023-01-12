1badfa3208d520b29981a8684e0aad6ee1a4ca02
Emre Erden <emreerden@pm.me>
2023-01-12 03:24:31 +0300

    added twelfth Questions-Algorithms

---
+++ Questions-Algorithms/Questions-Algorithms/12.js
@@ -0,0 +1,8 @@
+// Define variables using "prompt" and output using console.log.
+
+let fName = prompt("What is your first name?");
+let lName = prompt("What is your last name?");
+let age = prompt("What is your age?");
+
+console.log("Your name is " + fName + " " + lName); // output: Your name is ... ...
+console.log("You are " + age + " years old"); // output: You are ... years old
